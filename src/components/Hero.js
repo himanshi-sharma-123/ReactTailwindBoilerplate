e1c701c4ad3b2b5b372eaa9a4c98b43310a0fa08
Himanshi Sharma <himanshish456@gmail.com>
2024-04-10 21:41:44 +0530

    hero section responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="bg-[#ddd6fe] h-80">
-      <div className="items-center justify-center lg:ml-80 md:ml-40 ml-10 py-7">
+      <div className="items-center justify-center lg:ml-80 md:ml-40 py-7">
         <div className="lg:text-4xl md:text-4xl text-3xl font-bold lg:ml-60 md:ml-60 ml-20">
           Wix Premium Plans
         </div>
@@ -13,25 +13,27 @@ const Hero = () => {
             hosting FREE. Upgrade to Premium and get even more.
           </p>
         </div>
-        <div className="lg:ml-60 md:ml-60 mt-5 ml-20">
+        <div className="lg:ml-60 md:ml-60 mt-5 ml-10">
           <button className="bg-purple-500 text-white border-2 hover:bg-[#c85ad4] hover:text-black hover:border-black py-3 px-10 rounded-full cursor-pointer ml-10">
             Get Started Today
           </button>
         </div>
-        <div className="flex flex-row h-24 mt-5 px-10 ">
+        <div className="flex flex-row lg:h-24 mt-5 lg:px-10 ">
           <div className="bg-white lg:px-6 md:px-6 rounded-xl rounded-b-none">
             <h1 className="text-purple-500 lg:text-xl md:text-xl text-sm mt-4 lg:ml-20 md:ml-20 ml-10">
               Website Plans
             </h1>
-            <p className="text-purple-500 text-sm ">
+            <p className="text-purple-500 text-sm ml-5">
               Great for showcasing a professional site
             </p>
           </div>
           <div className="bg-blue-300 lg:px-6 md:px-6 rounded-b-none rounded-xl">
-            <h1 className="mt-4 lg:text-xl md:text-xl text-sm">
+            <h1 className="mt-4 lg:text-xl md:text-xl text-sm ml-3">
               Business and Ecommerce Plans
             </h1>
-            <p className="text-sm">Essential for accepting online payments</p>
+            <p className="text-sm ml-3">
+              Essential for accepting online payments
+            </p>
           </div>
         </div>
       </div>
